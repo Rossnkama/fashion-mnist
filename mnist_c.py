8775d251aee62e345b9d4a7cca889e36b3c599f4
Ross Nkama <rossnkama@Rosss-Air.default>
2018-07-23 21:02:00 +0100

    Added class names for plotting later

--- mnist_c.py
+++ mnist_c.py
@@ -4,8 +4,13 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-print(tf.__version__)
+print("tensorflow version:", tf.__version__)
 
 fashion_mnist = keras.datasets.fashion_mnist
-
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+# For reference later when plotting
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
