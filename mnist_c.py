36c7725278fa9310ebb736ec9c83b5abf205d816
Ross Nkama <rossnkama@Rosss-Air.default>
2018-07-24 00:45:19 +0100

    Added neural network layers

--- mnist_c.py
+++ mnist_c.py
@@ -13,4 +13,25 @@ fashion_mnist = keras.datasets.fashion_mnist
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
+# Data pre-processing
+""" To look at feature scales...
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid('off')
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
+"""
 
+# Feature scaling as largest scaling ranges from 0 (black) - 255 (white)
+train_images = train_images / 255
+test_images = test_images / 255
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
